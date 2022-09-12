f1deca151be40bcba35df0f32789f824552317e0
valentinpfeil <v.pfeil@outlook.com>
2022-09-12 07:31:23 +0200

    202209120727:
    * Checkstyles updated

--- test/triangle/TriangleTest.java
+++ test/triangle/TriangleTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 public class TriangleTest {
     @Test
     public void testTriangle() {
+        testTriangle();
+        System.out.println();
+
         Triangle t1 = new Triangle (2, 3, 1);
         System.out.println(t1.toString());
         System.out.println(t1.getRight());
